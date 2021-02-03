f1b1ef0f3e14cd865b9e3572f28ff79035d9a53f
victoelim <vlxa@outlook.com>
2021-02-03 22:26:23 +0800

    changed background

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -9,63 +9,15 @@ const homepage = () => {
         <>
             <div className = "hero">
                 <div className='hero-text'>
-                    <h2>Hi! Welcome</h2>
-                    <p>My name is Victor; an aspring web developer and this is my website. Please explore!</p>
+                    <h2>Hi! Welcome!</h2>
+                    <p>My name is Victor; an aspring web developer and this is my website. 
+                        Please explore! Hover around the items to see what they can do or click to pan around!</p>
                     
                 </div>
                 <div className="background">
-                    <svg className="big_purple" width="632" height="632" viewBox="0 0 632 632" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g filter="url(#filter0_f)">
-                            <circle cx="316" cy="316" r="199" fill="#C793EF"/>
-                        </g>
-                        <defs>
-                        <filter id="filter0_f" x="0" y="0" width="632" height="632" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                        <feGaussianBlur stdDeviation="58.5" result="effect1_foregroundBlur"/>
-                        </filter>
-                        </defs>
-                    </svg>
-                    <svg className="edge_red" width="203" height="365" viewBox="0 0 203 365" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g filter="url(#filter0_f)">
-                            <ellipse cx="182" cy="182.5" rx="89" ry="89.5" fill="#F18B8B"/>
-                        </g>
-                        <defs>
-                        <filter id="filter0_f" x="0" y="0" width="364" height="365" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                        <feGaussianBlur stdDeviation="46.5" result="effect1_foregroundBlur"/>
-                        </filter>
-                        </defs>
-                    </svg>
-                    <svg className="edge_yellow" width="198" height="341" viewBox="0 0 198 341" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g filter="url(#filter0_f)">
-                            <circle cx="27.5" cy="170.5" r="77.5" fill="#FAEB9B"/>
-                        </g>
-                        <defs>
-                        <filter id="filter0_f" x="-143" y="0" width="341" height="341" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                        <feGaussianBlur stdDeviation="46.5" result="effect1_foregroundBlur"/>
-                        </filter>
-                        </defs>
-                    </svg>
-                    <svg className="small_purple" width="372" height="372" viewBox="0 0 372 372" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g filter="url(#filter0_f)">
-                            <circle cx="186" cy="186" r="69" fill="#939DEF"/>
-                        </g>
-                        <defs>
-                        <filter id="filter0_f" x="0" y="0" width="372" height="372" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                        <feGaussianBlur stdDeviation="58.5" result="effect1_foregroundBlur"/>
-                        </filter>
-                        </defs>
-                    </svg>
-                    <img className="orange_rect" src={Orange_rect}/>
+                    <iframe src='https://my.spline.design/untitled-1f42cf61a008be14aed632a8b9acaa2b/' frameborder='0' width='100%' height='100%'></iframe>
                 </div>
                 
-
             </div>
             <section className="project-section">
                 <div className="projects">
