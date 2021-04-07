30fd22cbcac2fab1243d9d0aa08199b80e3f4a75
victoelim <vlxa@outlook.com>
2021-04-07 21:04:53 +0800

    added firebase to project and its config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import Navbar from "./component/Navbar";
 import Homepage from "./pages/homepage";
 import About from './pages/About';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey:`${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTHDOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASEURL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECTID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APPID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENTID}`
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
